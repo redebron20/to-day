212428dc37465343147430a494e21b48e4156d10
redebron20 <redebron20@gmail.com>
2021-02-11 12:51:30 -0500

    Build out Application Controller for homepage and add helper methods to enable sessions.

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
+# to-day
 
-To get this app running, clone this repo.
+to-day is a simple to-do-list app to help you organize your daily tasks
 
-Run bundle install then rake db:migrate then shotgun
+## Installation
+To get this app running, clone this repo and run bundle install in your terminal, then rake db:migrate, and finally shotgun.
+
+```bash
+bundle install
+rake db:migrate
+shotgun
+```
 
 Then navigate to localhost:9393 to try it!
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,39 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, ENV['SESSION_SECRET']
+    register Sinatra::Flash
   end
 
   get "/" do
     erb :welcome
+  end
+
+  error Sinatra::NotFound do
+    erb :"error.html"
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+    def redirect_if_not_logged_in
+        redirect "/login" if !logged_in?
+    end
+
+    def not_the_owner?(obj)
+      if current_user != obj.user
+        flash[:error] = "You do not have permission for that page!"
+        redirect '/coins' 
+      end
+    end
+
   end
 
 end
