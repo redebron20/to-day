db8c17a0c460715de1fc6d7863c726124027b3ed
redebron20 <redebron20@gmail.com>
2021-02-11 11:48:31 -0500

    Set up models (relationships and validations)

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 
+To get this app running, clone this repo.
+
+Run bundle install then rake db:migrate then shotgun
+
+Then navigate to localhost:9393 to try it!
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ActiveRecord::Base
     belongs_to :list
+    validates :name, presence: true, uniqueness: true
+    validates :list, presence: true
 end
