8427ee8c947e2c76a1d6d0fa50bafcbb7439f464
redebron20 <redebron20@gmail.com>
2021-02-17 11:04:48 -0500

    Build update route and form to edit list and add new tasks.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -23,19 +23,24 @@ class ListsController < ApplicationController
             task = list.tasks.create(params[:task])
         end
 
-        
-
-
-        erb :'tasks/show.html'
-
-        
+        erb :'tasks/show.html'  
     end
 
     get '/lists/:id/edit' do
         redirect_if_not_logged_in
         @list = List.find(params[:id])
+        erb :'lists/edit.html'
+    end
 
-        erb :'list/edit.html'
+    patch '/lists/:id' do
+        @list = List.find(params[:id])
+        @list.update(params["list"])
+
+        if !params["task"]["name"].empty?
+        @list.tasks << Task.create(params[:task])
+        end
+
+        redirect '/tasks'
     end
 
 end
