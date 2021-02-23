5a3be2cb89020ddd07f258ec9e3c23c310cf94c6
redebron20 <redebron20@gmail.com>
2021-02-23 17:58:15 -0500

    Change route /profile to /profiles.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,24 +1,24 @@
-class ProfileController < ApplicationController
-    before ("/profile/*") do
+class ProfilesController < ApplicationController
+    before ("/profiles/*") do
         redirect_if_not_logged_in
     end
 
-    get '/profile/:id' do
+    get '/profiles/:id' do
         if current_profile == Profile.find(params[:id])
             redirect '/profile'
         end
     end
 
-    get '/profile/:id/edit' do
+    get '/profiles/:id/edit' do
         erb :'profile/edit.html'
     end
 
-    patch '/profile/:id' do
+    patch '/profiles/:id' do
         if current_profile == Profile.find(params[:id])
             if current_profile.update(:name => params[:profile_name], :bio => params[:profile_bio])
-                redirect "/profile/#{current_profile.id}"
+                redirect "/profiles/#{current_profile.id}"
             else
-                redirect "/profile/edit"
+                redirect "/profiles/edit"
             end
         end
     end
--- config.ru
+++ config.ru
@@ -5,7 +5,7 @@ if ActiveRecord::Base.connection.migration_context.needs_migration?
 end
 
 use Rack::MethodOverride
-use ProfileController 
+use ProfilesController 
 use ListsController 
 use TasksController
 use SessionsController  
