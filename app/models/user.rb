71ba4c0b00914f2871db8de65749ca4d6cd6fc32
redebron20 <redebron20@gmail.com>
2021-02-25 02:52:20 -0500

    Add password confirmation on signup form and validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
     has_many :lists
     has_many :tasks, through: :lists
     has_secure_password
-    validates :password, length: {in: 4..20}
+    validates :password, length: {in: 4..20}, confirmation: true
     validates :username, presence: true, uniqueness: true, length: {in: 3..20}
 end
